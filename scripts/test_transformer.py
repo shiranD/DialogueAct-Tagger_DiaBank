e3a65ba7edf7f9e8d10b0715a11a4beaefca9816
Shiran Dudy <shdu9019@login12.rc.colorado.edu>
2022-09-08 17:20:16 -0600

    test files

--- scripts/test_transformer.py
+++ scripts/test_transformer.py
@@ -1,20 +1,25 @@
 import sys
 import os
-from taggers.transformer_tagger import BERT
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from corpora.maptask import Maptask
+from corpora.switchboard import Switchboard
+from corpora.ami import AMI
+from corpora.midas import MIDAS
+from corpora.daily_dialog import DailyDialog
+from taggers.transformer_tagger import BERT
 from tester import DialogueActTester
 from pathlib import Path
 
 
 if __name__ == "__main__":
-    model_path = ""
+    model_path = "/projects/shdu9019/DA_tagger/DialogueAct-Tagger_DiaBank/models/transformer_example"
     tester = DialogueActTester(
-        corpora_list=[
+        corpora=[
     #        (Maptask, str(Path("data/Maptask").resolve())),
     #        (AMI, str(Path("data/AMI/corpus").resolve())),
             (Switchboard, str(Path("data/Switchboard").resolve())),
     #        (DailyDialog, str(Path("data/DailyDialog").resolve())),
-        ], model_path
-    )
+        ], path=model_path)
     #sys.exit()
     t = tester.test()
